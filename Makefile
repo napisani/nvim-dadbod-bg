72ec3df3b090fa72e0abf5b9219f399ffe8863a6
Nick Pisani <napisani@yahoo.com>
2024-02-24 20:32:19 -0500

    release testing

--- Makefile
+++ Makefile
@@ -1,8 +1,13 @@
-all:
+all: clean buildweb buildgo 
+
+buildweb:
+	cd web && npm ci && npm run build 
+
+buildgo:
 	go build -o nvim-dadbod-bg .
 
 manifest:
 	./nvim-dadbod-bg -manifest nvim_dadbod_bg
 
 clean:
-	rm -f nvim-dadbod-bg nvim-dadbod-bg.log
+	rm -f nvim-dadbod-bg nvim-dadbod-bg.log && rm -rf web/dist
---
+++ install.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+GO_BIN=$(which go)
+MAKE_BIN=$(which make)
+if [[ -x "$GO_BIN" && -x "${MAKE_BIN}" ]]; then
+	echo "Go is installed - building from source"
+	make
+	exit 0
+else
+	echo "Go is not installed - downloading binary..."
+fi
+
+CURL_BIN=$(which curl)
+if [[ ! -x "$CURL_BIN" ]]; then
+	echo "curl is not installed - aborting install"
+	exit 1
+fi
+
+GIT_BIN=$(which git)
+if [[ ! -x "$GIT_BIN" ]]; then
+	echo "git is not installed - aborting install"
+	exit 1
+fi
+
+TAG=$(git describe --tags --abbrev=0)
+
+IS_MACOS=$(uname -s | grep -i Darwin)
+IS_LINUX=$(uname -s | grep -i Linux)
+ARCH=$(uname -m)
+echo "Detected OS: $IS_MACOS $IS_LINUX $ARCH"
+if [[ -n "$IS_MACOS" || -n "$IS_LINUX" ]] ; then
+	echo "Downloading binary for tag $TAG"
+	URL="https://github.com/napisani/nvim-dadbod-bg/releases/download/${TAG}/nvim-dadbod-bg_Darwin_${ARCH}"
+	curl -L -o nvim-dadbod-bg "$URL"
+	chmod +x nvim-dadbod-bg
+else
+	echo "Unsupported OS"
+	exit 1
+fi
