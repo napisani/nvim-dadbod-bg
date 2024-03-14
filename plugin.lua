90b244b35cabcead80bd01273ee12348657a6a61
Nick Pisani <napisani@yahoo.com>
2024-03-13 20:23:02 -0400

    lua

--- plugin.lua
+++ plugin.lua
@@ -1,3 +1,4 @@
 -- This file is unused but since this plugin is a remote neovim plugin (implemented in vimscript, golang and typescript) it does not have a lua implementation.
 -- However, I want it to be available on dotfyle.com, which has a constraint that all plugins must have a lua portion of the project on github.
 -- So, I am adding this file to satisfy the constraint.
+local _pluginNeverUsed = {}
