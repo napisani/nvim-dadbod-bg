4d5651d559c3fdbf1430f5bbdc95491552b4fbe0
Neil Simpson <neilsimp1@gmail.com>
2024-09-21 23:32:34 -0400

    Add basic support for copying cells and rows

--- web/src/Table.tsx
+++ web/src/Table.tsx
@@ -8,7 +8,7 @@ import { TableSettings } from './TableControls'
 import { AttributeMap, DataHeader, DataHeaders } from './query-results'
 import { parseByInferredType } from './utils'
 
-import { useMemo } from 'react'
+import { useMemo, useRef } from 'react'
 export interface TableProps {
   content: AttributeMap[]
   headers: DataHeaders
@@ -20,6 +20,7 @@ const paginationPageSizeSelector = [10, 20, 50, 100, 200, 500, 1000]
 export function Table({ content, headers, settings }: TableProps) {
   const columns = headers.map((header: DataHeader) => ({
     field: header.name,
+    editable: true,
   }))
 
   const data = useMemo(() => {
@@ -37,6 +38,58 @@ export function Table({ content, headers, settings }: TableProps) {
     )
   }, [content, headers])
 
+  const grid = useRef<AgGridReactType>(null)
+
+  const copyAllRowsToClipboard = _ => {
+    grid.current.api.selectAll()
+    copySelectedRowsToClipboard()
+  }
+
+  const copySelectedRowsToClipboard = _ => {
+    const copyFormat = document.getElementById('copyformat').value
+    const copyHeaders = document.getElementById('copyheaders').checked
+    const rows = grid.current.api.getSelectedRows()
+
+    const columnNames = grid.current.api.getAllDisplayedColumns()
+      .map(x => x.colId)
+
+    if (copyHeaders && copyFormat !== 'json') {
+      let row = {}
+      for (const name of columnNames) {
+        row[name] = name
+      }
+      rows.unshift(row)
+    }
+
+    let text = ''
+    switch (copyFormat) {
+      case 'html':
+        text = buildTableHtml(columnNames, rows)
+        break
+      case 'csv':
+        text = formatCsv(columnNames, rows)
+        break
+      case 'tab':
+        text = formatTabSeparated(columnNames, rows)
+        break
+      case 'json':
+        text = JSON.stringify(rows)
+        break
+    }
+
+    const span = document.createElement('span')
+    span.style.cssText = 'position:fixed;left:0;top:0;opacity:0;'
+    span.innerHTML = text
+    document.body.appendChild(span)
+    const selection = window.getSelection()
+    const range = document.createRange()
+    range.selectNodeContents(span)
+    selection.removeAllRanges()
+    selection.addRange(range)
+    document.execCommand('copy')
+    document.body.removeChild(span)
+  }
+  
   return (
     <div
       style={{
@@ -48,13 +101,51 @@ export function Table({ content, headers, settings }: TableProps) {
         style={{ height: `${settings.gridCellHeightPx}px` }} // the grid will fill the size of the parent container
       >
         <AgGridReact
+          ref={grid}
           rowData={data}
+          rowSelection={'multiple'}
           columnDefs={columns}
           pagination={true}
           paginationPageSize={paginationPageSize}
           paginationPageSizeSelector={paginationPageSizeSelector}
         />
       </div>
+
+      <button onClick={_ => copySelectedRowsToClipboard()}>Copy Selected Rows</button>
+      <button onClick={_ => copyAllRowsToClipboard()}>Copy All Rows</button>
+      <select id="copyformat">
+        <option value="html">HTML Table</option>
+        <option value="csv">CSV</option>
+        <option value="tab">Tab Separated</option>
+        <option value="json">JSON</option>
+      </select>
+      <label>
+        <input type="checkbox" id="copyheaders" defaultChecked />
+        Copy header values
+      </label>
     </div>
   )
+}
+
+const buildTableHtml = (columnNames, rows) => {
+  return '<table>' + rows
+    .map(x => '<tr>' + columnNames.map(y => `<td>${x[y]}</td>`).join('') + '</tr>').join('')
+    + '</table>'
+}
+
+const formatTabSeparated = (columnNames, rows) => {
+  return '<pre>' + rows
+    //.map(x => Object.keys(x).map(y => x[y]))
+    .map(x => columnNames.map(y => x[y]))
+    .map(x => x.join('\t'))
+    .join('\n')
+    + '</pre>'
+}
+
+const formatCsv = (columnNames, rows) => {
+  return '<pre>' + rows
+    .map(x => columnNames.map(y => `"${x[y.replace('"', '\"')]}"`))
+    .map(x => x.join(','))
+    .join('\n')
+    + '</pre>'
 }
